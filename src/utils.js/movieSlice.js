25d04793e22743389ae4483d393c8c7866de5a33
Krishan303 <krishan.pandey_cs19@gla.ac.in>
2025-03-18 12:59:58 +0530

    Update movieSlice.js

--- src/utils.js/movieSlice.js
+++ src/utils.js/movieSlice.js
@@ -1,29 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const moviesSlice = createSlice({
-    name: 'movies',
-    initialState:{
-        nowPlayingMovies: null,
-        addTrailer: null,
+  name: "movies",
+  initialState: {
+    nowPlayingMovies: null,
+    addTrailer: null,
+  },
+  reducers: {
+    addNowPlayingMovies: (state, action) => {
+      state.nowPlayingMovies = action.payload;
     },
-    reducers: {
-        addNowPlayingMovies: (state, action) => {
-            state.nowPlayingMovies = action.payload;
-        },
-        addPopularMovies: (state, action) => {
-            state.popularMovies = action.payload;
-        },
-        addTrendingMovies: (state, action) => {
-            state.trendingMovies = action.payload;
-        },
-        addUpcomingMovies: (state, action) => {
-            state.upcomingMovies = action.payload;
-        },
-        addMoviesTrailer: (state, action) => {
-            state.addTrailer = action.payload;
-        }
-    }
-})
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTrendingMovies: (state, action) => {
+      state.trendingMovies = action.payload;
+    },
+    addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
+    },
+    addMoviesTrailer: (state, action) => {
+      state.addTrailer = action.payload;
+    },
+  },
+});
 
-export const {addNowPlayingMovies, addMoviesTrailer, addPopularMovies, addTrendingMovies, addUpcomingMovies} = moviesSlice.actions;
+export const {
+  addNowPlayingMovies,
+  addMoviesTrailer,
+  addPopularMovies,
+  addTrendingMovies,
+  addUpcomingMovies,
+} = moviesSlice.actions;
 export default moviesSlice.reducer;
